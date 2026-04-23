5c03b39e3be982c703b8a0a47ff784eb752e36f0
khana15 <khana15@go.stockton.edu>
2026-04-23 09:36:35 -0400

    Create Unknown scene class in unknown.js
    Added a new Phaser scene class for the game.

---
+++ unknown.js
@@ -0,0 +1,19 @@
+// "Every great game begins with a single scene. Let's make this one unforgettable!"
+export class Unknown extends Phaser.Scene {
+    constructor() {
+        super('Unknown');
+    }
+
+    init() {
+        // Initialize scene
+    }
+
+    preload() {
+        // Load assets
+    }
+
+    create() {
+        // Create game objects
+    }this.load.image('key', 'path/to/image.png');
+
+}
